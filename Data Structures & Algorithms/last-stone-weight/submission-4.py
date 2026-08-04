8ede6cae51a6c074e8d88987c4deb0aa38794e5c
Suryansh Goyal <47456525+Suryanshg@users.noreply.github.com>
2026-08-04 11:06:24 -0400

    Add: last-stone-weight - submission-4

---
+++ Data Structures & Algorithms/last-stone-weight/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        # Create a max heap by using negating the stone values
+        max_heap = [-stone for stone in stones]
+        heapq.heapify(max_heap)
+
+        # While the max heap has more than 1 element
+        while len(max_heap) > 1:
+
+            # Pop x and y (x >= y, confirmed)
+            x = -heapq.heappop(max_heap)
+            y = -heapq.heappop(max_heap)
+
+            # If x > y (strictly), then push -(x - y)
+            if x > y:
+                heapq.heappush(max_heap, -(x - y))
+
+        # Return max heap's top element if it exists, else 0
+        return -max_heap[0] if max_heap else 0
