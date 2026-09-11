2d9dfb64b290adb1ac4fb13610276b205741224d
Suryansh Goyal <47456525+Suryanshg@users.noreply.github.com>
2026-09-11 15:31:00 -0400

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.py
@@ -0,0 +1,39 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Create a frequency map for each number
+        freq_map = {}
+        for num in nums:
+            if num in freq_map:
+                freq_map[num] += 1
+            else:
+                freq_map[num] = 1
+
+        # each num in nums can have frequency in range 0 - len(nums)
+        # We need len(nums) + 1 buckets of frequencies and in each bucket
+        # we keep what all elements of nums fall into
+        # For each idx in freq_buckets where idx is frequency, we store a set of
+        # elements of nums which have that frequency
+        freq_buckets = [set() for _ in range(len(nums) + 1)] 
+
+        # Now we populate the buckets
+        for num in nums:
+            # Get frequency of num
+            freq = freq_map[num]
+
+            # Add num to a bucket based on its freq
+            freq_buckets[freq].add(num)
+
+        # Now we find the top k frequent elements
+        top_k = []
+        for bucket in freq_buckets[::-1]: # Going in reverse order
+            # If we found top k, we can break out
+            if len(top_k) >= k:
+                break
+
+            # Add all items in the top k results from the current bucket
+            for item in bucket:
+                top_k.append(item)
+        
+        # Return the top k results
+        return top_k
+        
